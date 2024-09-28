9aae6a44c5b88587788a4c02fd24147edb25a144
Szalók Dániel <lockness91@gmail.com>
2024-09-28 08:58:24 +0200

    test

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/Work/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/Work/sqf_entity/example"
+export "FLUTTER_ROOT=D:\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Repositories\sqf_entity\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.2.1"
 export "FLUTTER_BUILD_NUMBER=1.2.1"
